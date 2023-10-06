6ff2b1849a19b41548d183ed8b38cc4c3c8674a7
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2023-10-06 22:50:04 +0800

    3. assignment and often operator

---
+++ basic_actions[1]/main.py
@@ -0,0 +1,72 @@
+# engiin uildluudig uzeh
+a = 12
+b = 3
+# nemeh
+sum = a + b
+# hasah
+sub = a - b
+# urjih
+mul = a * b
+# huwaah
+div = a / b
+# buhel hesgiig awch huwaah
+div1 = a // b
+# uldegdeltei huwaah
+mod = a % b
+print(sum, sub, mul, div, div1, mod)
+# geh met tsaash n sudlaarai
+
+# relational operator.
+# Bool utga butsaadag true or false
+# greater than
+print(a > b)
+# less than
+print (a < b)
+# equal to
+print(a == b)
+# ! - n uguisgeh temdeg.
+# Hereglee. not equal to
+print(a != b)
+# greater than or equal to
+print(a >= b)
+# less than or equal to
+print(a <= b)
+
+# logic operator
+isOdd = True
+isEven = False
+# logical and. buh nohtsol taarah shaardlagatai uyd ashiglana
+print(isOdd and isEven)
+# logical or. buh nohtsol taarah shaardlagagui zowhon 1n taarchihad boloh uyd ashiglana
+print(isOdd or isEven)
+# logical not. aliwaag esregeer n shalgah uyd ashiglana.
+print(not isEven)
+
+# assignment operator
+var = 25
+# add and
+var += 10 # ene n. var = var + 10 gsentei adilhan mon uund
+var -= 10
+var *= 10
+var /= 10
+# bj bolno oor ihenh uildluud der ch boldog.
+
+# Indetity Operator
+q = 1
+w = 1
+str = "hello"
+str1 = "Hello"
+# is n equal esehig shalgana
+print(q is w, str is str1)
+# is not gej bga ene n esregeer n gsen ug
+print(q is not w, str is not str1)
+
+# Membership operator
+# in - end bnu gej shalgana
+print('h' in 'my hair')
+# not in - end bnu gej shalgana
+print('h' not in 'my hair')
+
+# geh met
+
+
